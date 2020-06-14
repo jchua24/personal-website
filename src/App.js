7d35c78bcb165c1ea5caf2d4a2416b18730876fd
Joshua Chua <winwin.chua@gmail.com>
2020-06-13 23:03:46 -0400

    changed json link

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'/personal-website/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
